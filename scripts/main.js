42c41cd59d10c59b46a5d09a35d68773f1c81308
Lufy-wang <wwdqylh@tom.com>
2023-02-15 23:04:41 +0800

    website visit

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc === '../images/firefox-icon.png') {
-    myImage.setAttribute ('src','../images/firefox2.png');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute ('src','../images/firefox-icon.png');
+    myImage.setAttribute ('src','images/firefox-icon.png');
   }
 };
 
